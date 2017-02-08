6b5a7b4296424ebe21d2db127452d83894cf4267
Justin Randolph <Justin Randolph>
2017-02-07 21:28:24 -0600

    wedding website in its earliest stage

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// Countdown Timer
+var countDownDate = new Date("May 26, 2018 12:00:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML = days + " days, " + hours + " hours, "
+  + minutes + " minutes, and " + seconds + " seconds. ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+//end countdown timer
